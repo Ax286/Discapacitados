0fd1649ead6c2ed795fb186d327844813aae8df6
Ax286 <alex.rivera18@tectijuana.edu.mx>
2023-11-29 12:39:42 -0800

    api.py

--- Api/api.py
+++ Api/api.py
@@ -1,18 +1,45 @@
 from fastapi import FastAPI
-import json
-
+from fastapi.responses import JSONResponse
+from firebase_admin import credentials, initialize_app, db
+from fastapi import FastAPI
 
 app=FastAPI()
 
-#Leer archivo json
-with open('./json/Data.json','r') as myfile:
-    datos=myfile.read()
+cred=credentials.Certificate('.\json\discapacitados-54473-firebase-adminsdk-92ekf-f52b6a0147.json')
+firebase_app=initialize_app(cred,{'databaseURL':'https://discapacitados-54473-default-rtdb.firebaseio.com/'})
 
-Estados=json.loads(datos)
+app = FastAPI()
+
+# Rutas de ejemplo
+@app.get("/Data")
+def main():
+    # Lectura de datos desde Firebase
+    ref = db.reference('/')
+    data = ref.get()
+    return JSONResponse(content=data)
+
+@app.get("/Discapacitados")
+def discapacitados():
+    ref=db.reference('/Discapacitados')
+    data=ref.get()
+    return JSONResponse(content=data)
+
+@app.get("/Poblacion")
+def poblacion():
+    ref=db.reference('/Poblacion')
+    data=ref.get()
+    return JSONResponse(content=data)
+
+#Leer archivo json
+#with open('./json/Data.json','r') as myfile:
+#    datos=myfile.read()
+
+#Estados=json.loads(datos)
 
 #http://127.0.0.1:8000
 #uvicorn api:app
 
+"""
 #Creacion de metodos del API
 @app.get("/")
 def index():
@@ -45,3 +72,4 @@ def obtEdadHombres():
 @app.get("/Data/Discapacitados/Edades/Mujeres")
 def obtEdadMujeres():
     return Estados["Discapacitados"]["Edades"]["Mujeres"]
+"""
