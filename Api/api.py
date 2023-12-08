fd47822864c4bf8ad98c8a845a139bb9bfd6c288
Ax286 <alex.rivera18@tectijuana.edu.mx>
2023-12-08 14:45:55 -0800

    api update credentials

--- Api/api.py
+++ Api/api.py
@@ -3,9 +3,12 @@ from fastapi.responses import JSONResponse
 from firebase_admin import credentials, initialize_app, db
 from fastapi import FastAPI
 
+#http://127.0.0.1:8000
+#uvicorn api:app
+
 app=FastAPI()
 
-cred=credentials.Certificate('.\json\discapacitados-54473-firebase-adminsdk-92ekf-f52b6a0147.json')
+cred=credentials.Certificate('./json/discapacitados-54473-firebase-adminsdk-92ekf-f52b6a0147.json')
 firebase_app=initialize_app(cred,{'databaseURL':'https://discapacitados-54473-default-rtdb.firebaseio.com/'})
 
 app = FastAPI()
@@ -30,46 +33,3 @@ def poblacion():
     data=ref.get()
     return JSONResponse(content=data)
 
-#Leer archivo json
-#with open('./json/Data.json','r') as myfile:
-#    datos=myfile.read()
-
-#Estados=json.loads(datos)
-
-#http://127.0.0.1:8000
-#uvicorn api:app
-
-"""
-#Creacion de metodos del API
-@app.get("/")
-def index():
-    return "Main"
-
-@app.get("/Data/")
-def obtener_estados():
-    return Estados
-
-@app.get("/Data/Discapacitados/General")
-def obGeneral():
-    return Estados["Discapacitados"]["General"]
-
-@app.get("/Data/Poblacion/")
-def obPoblacion():
-    return Estados["Poblacion"]
-
-@app.get("/Data/Discapacitados/Tipo/Hombres")
-def obTipoHombres():
-    return Estados["Discapacitados"]["Tipo"]["Hombres"]
-
-@app.get("/Data/Discapacitados/Tipo/Mujeres")
-def obTipoMujeres():
-    return Estados["Discapacitados"]["Tipo"]["Mujeres"]
-
-@app.get("/Data/Discapacitados/Edades/Hombres")
-def obtEdadHombres():
-    return Estados["Discapacitados"]["Edades"]["Hombres"]
-
-@app.get("/Data/Discapacitados/Edades/Mujeres")
-def obtEdadMujeres():
-    return Estados["Discapacitados"]["Edades"]["Mujeres"]
-"""
