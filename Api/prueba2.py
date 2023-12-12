05df913e9229eddbc5e2aad9129fb9aa84e0be66
Ax286 <alex.rivera18@tectijuana.edu.mx>
2023-12-12 14:44:59 -0800

    update about y modelo

--- Api/prueba2.py
+++ Api/prueba2.py
@@ -3,8 +3,11 @@ from fastapi.responses import JSONResponse
 from firebase_admin import credentials, initialize_app, db
 from fastapi import FastAPI
 
-peticion=requests.get('http://127.0.0.1:8000/Data')
+cred=credentials.Certificate('Api/json/discapacitados-54473-firebase-adminsdk-92ekf-f52b6a0147.json')
+firebase_app=initialize_app(cred,{'databaseURL':'https://discapacitados-54473-default-rtdb.firebaseio.com/'})
 
-if(peticion.status_code==200):
-    data=peticion.json()
-    print(data)
+ref=db.reference('/')
+
+data=ref.get()
+
+print(JSONResponse(content=data))
